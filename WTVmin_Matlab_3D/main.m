0566f1dbf5de9d2130dd10d083cd069162facb01
Ali Ghayoor <ali-ghayoor@uiowa.edu>
2016-06-07 15:15:52 -0500

    ENH: fourier projection operator works on not square images

--- WTVmin_Matlab_3D/main.m
+++ WTVmin_Matlab_3D/main.m
@@ -6,12 +6,12 @@ load 'edgemask.mat'
 
 X0 = double(inputImage);
 X0_size = size(X0);
-%X0 = X0(:,:,round(X0_size(3)/2)); % make input 2D %%%%%%%%%%%%%
-X0 = squeeze( X0(:,round(X0_size(2)/2),:) ); 
+X0 = X0(:,:,round(X0_size(3)/2)); % make input 2D %%%%%%%%%%%%%
+%X0 = squeeze( X0(:,round(X0_size(2)/2),:) );
 
 edgemask_size = size(edgemask);
-%edgemask = edgemask(:,:,round(edgemask_size(3)/2)); % make edgemask 2D %%%%%%%%%%%%
-edgemask = squeeze( edgemask(:,round(edgemask_size(2)/2),:) );
+edgemask = edgemask(:,:,round(edgemask_size(3)/2)); % make edgemask 2D %%%%%%%%%%%%
+%edgemask = squeeze( edgemask(:,round(edgemask_size(2)/2),:) );
 
 figure(1); imagesc(abs(X0),[0,1]); colorbar; title('ground truth');
 %figure(1); imshow(X0,[0,1]); title('ground truth');
@@ -31,7 +31,7 @@ res = inres; %output resolution
 lores = round(inres/2); %input lower resolution (use odd numbers)
 
 ind_samples = get_lowpass_inds(k,lores);
-[A,At] = defAAt_fourier(ind_samples, res); %Define function handles for fourier projection operators                                                
+[A,At] = defAAt_fourier(ind_samples, res); %Define function handles for fourier projection operators
 b = A(X0);       %low-resolution fourier samples
 Xlow = ifft2(reshape(b,lores));
 figure(3); imagesc(abs(Xlow)); colorbar; title('low resolution input');
@@ -66,7 +66,7 @@ fprintf('TV output SNR = %2.1f dB\n',SNR_TV);
 lambda = 3e-2; %regularization parameter (typically in the range [1e-2,1], if original image scaled to [0,1])
 Niter = 200;  %number of iterations (typically 500-1000 for Fourier inversion)
 [X_WTV, cost] = OpWeightedTV_PD_AHMOD(b,edgemask,lambda,A,At,res,Niter); %see comments inside OpWeightedTV_PD_AHMOD.m
-figure(7); imagesc(abs(X_WTV),[0,1]); colorbar; title('hi resolution, WTV new');
+figure(7); imagesc(abs(X_WTV),[0,1]); colorbar; title('hi resolution, WTV');
 figure(8); plot(cost); xlabel('iteration'); ylabel('cost');
 SNR_WTV = -20*log10(norm(X_WTV(:)-X0(:))/norm(X0(:)));
 fprintf('WTV output SNR (Weighted TV algorithm) = %2.1f dB\n',SNR_WTV);
