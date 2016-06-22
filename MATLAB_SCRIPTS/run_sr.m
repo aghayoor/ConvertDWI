bb889711b8672fa589a9218b123fab4597254804
Ali Ghayoor <ali-ghayoor@uiowa.edu>
2016-06-21 20:52:29 -0500

    WIP: started actual changes

--- MATLAB_SCRIPTS/run_sr.m
+++ MATLAB_SCRIPTS/run_sr.m
@@ -1,82 +1,44 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%This function uses compressed sensing to recover
-%the HARDI signal (single shell) from very vew gradient directions.
-%A minimum of 16 directions are needed to recover crossing fiber signal.
+%This function run following super resolution methods on a dwi image
+% - Nearest Neighbor
+% - zero-padded IFFT
+% - Total Variation
+% - Edge guided Weighted Total Variation (WTV)
 %
 % Input: 'input_dwi_fn' filename in nhdr format
 %      : 'input_mask_fn' name of the mask file
-%      : 'output_dwi_fn' name of the output filename to store the output
-%      : 'denoising_factor' A parameter for adjusting how much de-noising 
-%           is done at each iteration 0=no denoising, 0.025 for very little
-%           denoising, and 0.05 for "typical scans" level of noise.
-%      : 'new_gradients' optionally, supply the new gradients on which to
-%           sample the recovered signal
-%      : 'saveC' - optionally, save the coefficients for future use.
-% Output: the recovered diffusion signal data file in nhdr format.
+%      : 'output_dwi_dir' name of the output directory to store the output
 %
-% Yogesh Rathi, Oleg Michailovich, June 20th 2011
+% Output: the recovered diffusion signal data files in nrrd format.
+%
+% Ali Ghayoor, Hans J. Johnson, Greg Ongie, University of Iowa, June 2016
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function run_cs(input_dwi_fn,input_mask_fn,output_dwi_fn,denoising_factor,new_gradients,saveC)
-    % Add main MATLAB_SCRIPTS code tree
-    thisFile = mfilename('fullpath');
-    addpath(fullfile(fileparts(thisFile),'support_functions'));
+function run_sr(input_dwi_fn, input_mask_fn, output_dwi_dir)
+    
+% Add main MATLAB_SCRIPTS code tree
+thisFile = mfilename('fullpath');
+addpath(fullfile(fileparts(thisFile),'support_functions'));
 
-fprintf('Starting testCS:\n    input_dwi_fn: %s\n    input_mask_fn: %s\n    output_dwi_fn: %s\n',input_dwi_fn,input_mask_fn,output_dwi_fn)
+fprintf('Starting testCS:\n    input_dwi_fn: %s\n    input_mask_fn: %s\n    output_dwi_dir: %s\n',input_dwi_fn,input_mask_fn,output_dwi_dir
 
-
-%
-% KW QUESTION: How do the default gradients relate to the DWI magnitude images?
-% Just choosing a bunch of icosahedron vertices as gradients without doing some
-% of manipulation of the magnitudes means that you are generating a nonsense DWI output
-% file.
-if nargin >=5 && new_gradients
-    nu = new_gradients;
-else
-    nu = SetDefaultGradients();
-end
 
 [ rawDWI ] = nrrdLoadWithMetadata(input_dwi_fn);
-[ reformattedDWI, voxelLatticeToAnatomicalSpace ] = nrrdReformatAndNormalize(rawDWI);
-[ dwi_struct, metric, counts ] = BalanceDWIReplications( reformattedDWI );
+%[ reformattedDWI, voxelLatticeToAnatomicalSpace ] = nrrdReformatAndNormalize(rawDWI);
+%[ dwi_struct, metric, counts ] = BalanceDWIReplications( reformattedDWI );
+
+% read input mask file
 in_mask = nrrdLoadWithMetadata(input_mask_fn);
-mask = in_mask.data ~= 0;
+%mask = in_mask.data ~= 0;
 
-% change nu to be aligned in voxel lattice space
-% This cause the anatomicalEstimatedGradients be the same for all output
-% data after CS computations.
-gradientsInVoxelLatticeSpace = (voxelLatticeToAnatomicalSpace\nu')';
-
-%save('/tmp/before_doCSestimate.mat','-v7.3')
-[estimatedSignal,estimatedGradients] = doCSestimate(dwi_struct, mask, gradientsInVoxelLatticeSpace, denoising_factor);
-
-anatomicalEstimatedGradients = ( voxelLatticeToAnatomicalSpace*estimatedGradients' )';
+[estimatedSignal] = doSRestimate(dwi_struct, mask);
 
 nrrdStrct = dwi_struct;
-nrrdStrct.data=single(estimatedSignal);
-nrrdStrct.gradientdirections=anatomicalEstimatedGradients;
+nrrdStrct.data = estimatedSignal;
+%nrrdStrct.gradientdirections = estimatedGradients;
 
-fprintf('Writing file to disk...\n');
+fprintf('Writing files to disk...\n');
 
 nrrdSaveWithMetadata(output_dwi_fn,nrrdStrct)
-if nargin == 5 && saveC
-  save(output_fn,'c','-v7.3');
-end
-end
 
-function [ nu ] =  SetDefaultGradients()
-    % icosahedron produces a antipodal symmetric set of points,
-    % so remove the redundant samples.
-
-    % Add main MATLAB_SCRIPTS code tree
-    thisFile = mfilename('fullpath');
-    addpath(fullfile(fileparts(thisFile),'support_functions'));
-
-    %ico1 = icosahedron(1); %42 gradient direction
-    ico2 = icosahedron(2); %162  <-- Good default for most cases
-    %ico3 = icosahedron(3); %642
-    nu = ico2; %162 gradient directions
-
-    n0 = size(nu,1)/2;
-    nu = nu(1:n0,:); %81 gradient directions
 end
---
+++ MATLAB_SCRIPTS/test_runSR.m
@@ -0,0 +1,91 @@
+clear all
+close all
+
+load 'dwi_b0.mat'
+load 'edgemask.mat'
+
+X0 = double(inputImage);
+X0_size = size(X0);
+X0_2d = X0(:,:,round(X0_size(3)/2)); % make input 2D %%%%%%%%%%%%%
+%X0_2d = squeeze( X0(:,round(X0_size(2)/2),:) );
+
+edgemask_size = size(edgemask);
+edgemask_2d = edgemask(:,:,round(edgemask_size(3)/2)); % make edgemask 2D %%%%%%%%%%%%
+%edgemask_2d = squeeze( edgemask(:,round(edgemask_size(2)/2),:) );
+
+figure(1); imagesc(abs(X0_2d),[0,1]); colorbar; title('ground truth');
+%figure(1); imshow(X0_2d,[0,1]); title('ground truth');
+figure(2); imagesc(edgemask_2d,[0,0.1]); colorbar; title('spatial weights image');
+%figure(2); imshow(edgemask_2d,[0 0.1]); title('spatial weights image');
+
+%%
+% Test algorithm on 2D images, then if verified, expand it to 3D
+%
+%%
+m = fftn(X0);       %m=fourier data
+inres = size(m);
+k = get_kspace_inds( inres ); %k=fourier indices
+
+%% Define Fourier Projection Operators
+res = inres; %output resolution
+lores = round(inres/2); %input lower resolution (use odd numbers)
+
+ind_samples = get_lowpass_inds(k,lores);
+[A,At] = defAAt_fourier(ind_samples, res); %Define function handles for fourier projection operators
+b = A(X0);       %low-resolution fourier samples
+Xlow = ifftn(reshape(b,lores));
+% Show a 2D slice of low resolution image
+Xlow_size = size(Xlow);
+Xlow_2d = Xlow(:,:,round(Xlow_size(3)/2));
+figure(3); imagesc(abs(Xlow_2d)); colorbar; title('low resolution input');
+
+%% Zero-padded IFFT reconstruction
+X_IFFT = At(fftn(Xlow));
+% Show a 2D slice of zero-padded IFFT image
+X_IFFT_size = size(X_IFFT);
+X_IFFT_2d = X_IFFT(:,:,round(X_IFFT_size(3)/2));
+figure(4); imagesc(abs(X_IFFT_2d),[0,1]); colorbar; title('hi resolution, zero-padded IFFT');
+
+SNR_IFFT = -20*log10(norm(X_IFFT(:)-X0(:))/norm(X0(:)));
+fprintf('Zero-padded IFFT output SNR = %2.1f dB\n',SNR_IFFT);
+
+%% Run Standard TV algorithm (no edgemask) -- new implementation
+% lambda = 1e-4; %regularization parameter (typically in the range [1e-2,1], if original image scaled to [0,1])
+% Niter = 100;  %number of iterations (typically 20-100 for Fourier inversion)
+% [X_TV, cost] = OpTV_AL(b,lambda,A,At,res,Niter);
+% figure(5); imagesc(abs(X_TV),[0,1]); colorbar; title('hi resolution, TV (no edgemask)');
+% figure(6); plot(cost); xlabel('iteration'); ylabel('cost');
+% SNR_TV = -20*log10(norm(X_TV(:)-X0(:))/norm(X0(:)));
+% fprintf('TV output SNR = %2.1f dB\n',SNR_TV);
+
+%% Run Standard TV algorithm (no edgemask)
+lambda = 5e-4; %regularization parameter
+Niter = 25;  %number of iterations (typically 500-1000 for Fourier inversion)
+siz = size(edgemask);
+edgemask_1 = ones(siz);
+tic
+[X_TV, cost] = OpWeightedTV_PD_AHMOD(b,edgemask_1,lambda,A,At,res,Niter);
+toc
+% Show a 2D slice of X_TV image
+X_TV_size = size(X_TV);
+X_TV_2d = X_TV(:,:,round(X_TV_size(3)/2));
+figure(5); imagesc(abs(X_TV_2d),[0,1]); colorbar; title('hi resolution, TV (no edgemask)');
+figure(6); plot(cost); xlabel('iteration'); ylabel('cost');
+
+SNR_TV = -20*log10(norm(X_TV(:)-X0(:))/norm(X0(:)));
+fprintf('TV output SNR = %2.1f dB\n',SNR_TV);
+
+%% Run New Weighted TV algorithm
+lambda = 5e-4; %regularization parameter (typically in the range [1e-2,1], if original image scaled to [0,1])
+Niter = 25;  %number of iterations (typically 500-1000 for Fourier inversion)
+tic
+[X_WTV, cost] = OpWeightedTV_PD_AHMOD(b,edgemask,lambda,A,At,res,Niter); %see comments inside OpWeightedTV_PD_AHMOD.m
+toc
+% Show a 2D slice of X_WTV image
+X_WTV_size = size(X_WTV);
+X_WTV_2d = X_WTV(:,:,round(X_WTV_size(3)/2));
+figure(7); imagesc(abs(X_WTV_2d),[0,1]); colorbar; title('hi resolution, WTV');
+figure(8); plot(cost); xlabel('iteration'); ylabel('cost');
+
+SNR_WTV = -20*log10(norm(X_WTV(:)-X0(:))/norm(X0(:)));
+fprintf('WTV output SNR (Weighted TV algorithm) = %2.1f dB\n',SNR_WTV);
