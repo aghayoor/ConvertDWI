539493db3b1a90268869dd5e3a05561ff84c8c96
Ali Ghayoor <ali-ghayoor@uiowa.edu>
2016-06-30 13:44:42 -0500

    WIP: run 2d test for hcp
    For best parameters sets:
    zero-padded IFFT: 21.1
    TV_AL: 21.9
    TV_AHMOD: 22.3
    WTV_AHMOD: 23.6

--- OldMatlabFiles/WTVmin_Matlab_2D/test_realdata_fri.m
+++ OldMatlabFiles/WTVmin_Matlab_2D/test_realdata_fri.m
@@ -4,13 +4,21 @@ close all
 % Test of Weighted TV algorithm for super-resolution reconstruction of 2D MRI data.
 %
 %% Load Data
-load Data/dwib0_testdata;
+% proposal test data
+%load Data/dwib0_testdata
 %load Data/dwig1_testdata;
 
+% HCP test data
+load /scratch/TESTS/IpythonNotebook/20160615_HCPWF/2_SRWF/test_tune_parameters/matlabFiles/dwib0_testdata;
+
 %% Load Weights
+% proposal masks
 %%%load Data/t2_edgemask_fri;   %edgemask estimated in stage one
-load /scratch/TESTS/Aim2/MatlabFiles/edgemask_t1t2_1ByGMI.mat
+%load /scratch/TESTS/Aim2/MatlabFiles/edgemask_t1t2_1ByGMI.mat
 %%%load /scratch/TESTS/Aim2/MatlabFiles/edgemask_t1t2_1ByGMI_square.mat
+
+% HCP mask
+load /scratch/TESTS/IpythonNotebook/20160615_HCPWF/2_SRWF/test_tune_parameters/matlabFiles/edgemask_t1t2_1ByGMI.mat
 
 %%
 %%%%%%%%%%%%%%%
@@ -31,7 +39,7 @@ inres = size(m);
 k = get_kspace_inds( inres ); %k=fourier indices
 
 figure(1); imagesc(abs(X0),[0,1]); colorbar; title('ground truth');
-figure(2); imshow(edgemask,[0 1]); title('spatial weights image');
+figure(2); imagesc(edgemask,[0 1]); colorbar; title('spatial weights image');
 
 %% Define Fourier Projection Operators
 res = inres; %output resolution
@@ -59,20 +67,20 @@ fprintf('Zero-padded IFFT output SNR = %2.1f dB\n',SNR_IFFT);
 % fprintf('TV output SNR = %2.1f dB\n',SNR_TV);
 
 %% Run Standard TV algorithm (no edgemask)
-lambda = 8e-4; %regularization parameter (typically in the range [1e-2,1], if original image scaled to [0,1])
+lambda = 5e-4; %regularization parameter (typically in the range [1e-2,1], if original image scaled to [0,1])
 Niter = 200;  %number of iterations (typically 20-100 for Fourier inversion)
 siz = size(edgemask);
 edgemask_1 = ones(siz);
-[X_TV, cost] = OpWeightedTV_PD_AHMOD(b,edgemask_1,lambda,A,At,res,Niter);
+[X_TV, cost] = OpWeightedTV_PD_AHMOD2(b,edgemask_1,lambda,A,At,res,Niter);
 figure(5); imagesc(abs(X_TV),[0,1]); colorbar; title('hi resolution, TV (no edgemask)');
 figure(6); plot(cost); xlabel('iteration'); ylabel('cost');
 SNR_TV = -20*log10(norm(X_TV(:)-X0(:))/norm(X0(:)));
 fprintf('TV output SNR = %2.1f dB\n',SNR_TV);
 
 %% Run New Weighted TV algorithm
-lambda = 3e-2; %regularization parameter (typically in the range [1e-2,1], if original image scaled to [0,1])
-Niter = 200;  %number of iterations (typically 500-1000 for Fourier inversion)
-[X_WTV, cost] = OpWeightedTV_PD_AHMOD(b,edgemask,lambda,A,At,res,Niter); %see comments inside OpWeightedTV_PD_AHMOD.m
+lambda = 5e-3; %regularization parameter (typically in the range [1e-2,1], if original image scaled to [0,1])
+Niter = 400;  %number of iterations (typically 500-1000 for Fourier inversion)
+[X_WTV, cost] = OpWeightedTV_PD_AHMOD2(b,edgemask,lambda,A,At,res,Niter); %see comments inside OpWeightedTV_PD_AHMOD.m
 figure(7); imagesc(abs(X_WTV),[0,1]); colorbar; title('hi resolution, WTV new');
 figure(8); plot(cost); xlabel('iteration'); ylabel('cost');
 SNR_WTV = -20*log10(norm(X_WTV(:)-X0(:))/norm(X0(:)));
