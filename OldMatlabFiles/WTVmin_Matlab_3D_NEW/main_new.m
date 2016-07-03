359983bab2c43c74cfb40b1b253bff09f892d8c0
Ali Ghayoor <ali-ghayoor@uiowa.edu>
2016-07-02 19:19:56 -0500

    WIP: refactored main_new

--- OldMatlabFiles/WTVmin_Matlab_3D_NEW/main_new.m
+++ OldMatlabFiles/WTVmin_Matlab_3D_NEW/main_new.m
@@ -45,59 +45,40 @@ SNR_IFFT = -20*log10(norm(X_IFFT(:)-X0(:))/norm(X0(:)));
 fprintf('Zero-padded IFFT output SNR = %2.1f dB\n',SNR_IFFT);
 
 %% Run Standard TV algorithm (no edgemask) -- ADMM algorithm
-lambda = 1e-4; %regularization parameter (typically in the range [1e-2,1], if original image scaled to [0,1])
-Niter = 100;  %number of iterations (typically 20-100 for Fourier inversion)
-beta = 10; %ADMM parameter
-tic
-[X_TV_AL, cost] = OpTV_AL_3D(b,lambda,A,At,res,Niter,beta);
-toc
-X_TV_AL_size = size(X_TV_AL);
-X_TV_AL_2d = X_TV_AL(:,:,round(X_TV_AL_size(3)/2));
-figure(5); imagesc(abs(X_TV_AL_2d),[0,1]); colorbar; title('hi resolution, TV_AL (no edgemask)');
-figure(6); plot(cost); xlabel('iteration'); ylabel('cost');
-figure(7); imagesc(abs(X_TV_AL_2d-X0_2d),[0,0.2]); colorbar; title('TV_AL error image');
+% lambda = 1e-4; %regularization parameter (typically in the range [1e-2,1], if original image scaled to [0,1])
+% Niter = 100;  %number of iterations (typically 20-100 for Fourier inversion)
+% beta = 10; %ADMM parameter
+% tic
+% [X_TV_AL, cost] = OpTV_AL_3D(b,lambda,A,At,res,Niter,beta);
+% toc
+% X_TV_AL_size = size(X_TV_AL);
+% X_TV_AL_2d = X_TV_AL(:,:,round(X_TV_AL_size(3)/2));
+% figure(5); imagesc(abs(X_TV_AL_2d),[0,1]); colorbar; title('hi resolution, TV_AL (no edgemask)');
+% figure(6); plot(cost); xlabel('iteration'); ylabel('cost');
+% figure(7); imagesc(abs(X_TV_AL_2d-X0_2d),[0,0.2]); colorbar; title('TV_AL error image');
+%
+% SNR_TV_AL = -20*log10(norm(X_TV_AL(:)-X0(:))/norm(X0(:)));
+% fprintf('TV_AL (ADMM) output SNR = %2.1f dB\n',SNR_TV_AL);
+% fprintf('TV_AL (ADMM) final cost %6.4f\n',cost(end));
 
-SNR_TV_AL = -20*log10(norm(X_TV_AL(:)-X0(:))/norm(X0(:)));
-fprintf('TV_AL (ADMM) output SNR = %2.1f dB\n',SNR_TV_AL);
-fprintf('TV_AL (ADMM) final cost %6.4f\n',cost(end));
-
-%% Run Standard TV algorithm (no edgemask) -- AHMOD from Chambolle & Pock
-lambda = 1e-4; %regularization parameter (typically in the range [1e-2,1], if original image scaled to [0,1])
-Niter = 200;  %number of iterations (typically 500-1000 for Fourier inversion)
-gam = 0.01; %set in the range [0.01,1].
-Xinit = real(At(b)); %initialization
+%% Run Weighted L2 algorithm - ADMM version
+lambda = 1e-3; %regularization parameter
+Niter = 100;  %number of iterations
+gam = 1;   %ADMM parameter, in range [0.1,10]
+tol = 1e-8;  %convergence tolerance
 tic
-[X_TV_PD, cost] = OpWeightedTV_PD_AHMOD2(b,ones(res),lambda,A,At,res,Niter,gam,Xinit); %see comments inside OpWeightedTV_PD_AHMOD.m
+[X_L2, cost] = OpWeightedL2(b,ones(res),lambda,A,At,res,Niter,tol,gam);
 toc
-% Show a 2D slice of X_WTV image
-X_TV_PD_size = size(X_TV_PD);
-X_TV_PD_2d = X_TV_PD(:,:,round(X_TV_PD_size(3)/2));
-figure(8); imagesc(abs(X_TV_PD_2d),[0,1]); colorbar; title('hi resolution, TV_PD');
+% Show a 2D slice
+X_L2_size = size(X_L2);
+X_L2_2d = X_L2(:,:,round(X_L2_size(3)/2));
+figure(8); imagesc(abs(X_L2_2d),[0,1]); colorbar; title('hi resolution, L2');
 figure(9); plot(cost); xlabel('iteration'); ylabel('cost');
-figure(10); imagesc(abs(X_TV_PD_2d-X0_2d),[0,0.2]); colorbar; title('TV_PD error image');
+figure(10); imagesc(abs(X_L2_2d-X0_2d),[0,0.2]); colorbar; title('L2 error image');
 
-SNR_TV_PD = -20*log10(norm(X_TV_PD(:)-X0(:))/norm(X0(:)));
-fprintf('TV_PD (AHMOD) output SNR = %2.1f dB\n',SNR_TV_PD);
-fprintf('TV_PD (AHMOD) final cost %6.4f\n',cost(end));
-
-%% Run Weighted TV algorithm - Primal-Dual algorithm -- AHMOD from Chambolle & Pock
-lambda = 1e-3; %regularization parameter (typically in the range [1e-3,1], if original image scaled to [0,1])
-Niter = 500;  %number of iterations (typically 500-1000 for Fourier inversion)
-gam = 0.01; %set in the range [0.01,1].
-Xinit = real(At(b)); %initialization
-tic
-[X_WTV, cost] = OpWeightedTV_PD_AHMOD2(b,sqrt(edgemask),lambda,A,At,res,Niter,gam,Xinit); %see comments inside OpWeightedTV_PD_AHMOD.m
-toc
-% Show a 2D slice of X_WTV image
-X_WTV_size = size(X_WTV);
-X_WTV_2d = X_WTV(:,:,round(X_WTV_size(3)/2));
-figure(11); imagesc(abs(X_WTV_2d),[0,1]); colorbar; title('hi resolution, WTV');
-figure(12); plot(cost); xlabel('iteration'); ylabel('cost');
-figure(13); imagesc(abs(X_WTV_2d-X0_2d),[0,0.2]); colorbar; title('WTV error image');
-
-SNR_WTV = -20*log10(norm(X_WTV(:)-X0(:))/norm(X0(:)));
-fprintf('WTV (AHMOD) output SNR (Weighted TV algorithm) = %2.1f dB\n',SNR_WTV);
-fprintf('WTV (AHMOD) final cost %6.4f\n',cost(end));
+SNR_L2 = -20*log10(norm(X_L2(:)-X0(:))/norm(X0(:)));
+fprintf('L2 output SNR (L2 algorithm) = %2.1f dB\n',SNR_L2);
+fprintf('L2 final cost %2.4e\n',cost(end));
 
 %% Run Weighted L2 algorithm - ADMM version
 lambda = 1e-3; %regularization parameter
@@ -110,13 +91,51 @@ toc
 % Show a 2D slice
 X_WL2_size = size(X_WL2);
 X_WL2_2d = X_WL2(:,:,round(X_WL2_size(3)/2));
-figure(14); imagesc(abs(X_WL2_2d),[0,1]); colorbar; title('hi resolution, WL2');
-figure(15); plot(cost); xlabel('iteration'); ylabel('cost');
-figure(16); imagesc(abs(X_WL2_2d-X0_2d),[0,0.2]); colorbar; title('WL2 error image');
+figure(11); imagesc(abs(X_WL2_2d),[0,1]); colorbar; title('hi resolution, WL2');
+figure(12); plot(cost); xlabel('iteration'); ylabel('cost');
+figure(13); imagesc(abs(X_WL2_2d-X0_2d),[0,0.2]); colorbar; title('WL2 error image');
 
 SNR_WL2 = -20*log10(norm(X_WL2(:)-X0(:))/norm(X0(:)));
 fprintf('WL2 output SNR (Weighted L2 algorithm) = %2.1f dB\n',SNR_WL2);
 fprintf('WL2 final cost %2.4e\n',cost(end));
+
+%% Run Standard TV algorithm (no edgemask) -- AHMOD from Chambolle & Pock
+lambda = 1e-4; %regularization parameter (typically in the range [1e-2,1], if original image scaled to [0,1])
+Niter = 200;  %number of iterations (typically 500-1000 for Fourier inversion)
+gam = 0.01; %set in the range [0.01,1].
+Xinit = real(At(b)); %initialization
+tic
+[X_TV_PD, cost] = OpWeightedTV_PD_AHMOD2(b,ones(res),lambda,A,At,res,Niter,gam,Xinit); %see comments inside OpWeightedTV_PD_AHMOD.m
+toc
+% Show a 2D slice of X_WTV image
+X_TV_PD_size = size(X_TV_PD);
+X_TV_PD_2d = X_TV_PD(:,:,round(X_TV_PD_size(3)/2));
+figure(14); imagesc(abs(X_TV_PD_2d),[0,1]); colorbar; title('hi resolution, TV_PD');
+figure(15); plot(cost); xlabel('iteration'); ylabel('cost');
+figure(16); imagesc(abs(X_TV_PD_2d-X0_2d),[0,0.2]); colorbar; title('TV_PD error image');
+
+SNR_TV_PD = -20*log10(norm(X_TV_PD(:)-X0(:))/norm(X0(:)));
+fprintf('TV_PD (AHMOD) output SNR = %2.1f dB\n',SNR_TV_PD);
+fprintf('TV_PD (AHMOD) final cost %6.4f\n',cost(end));
+
+%% Run Weighted TV algorithm - Primal-Dual algorithm -- AHMOD from Chambolle & Pock
+lambda = 1e-3; %regularization parameter (typically in the range [1e-3,1], if original image scaled to [0,1])
+Niter = 400;  %number of iterations (typically 500-1000 for Fourier inversion)
+gam = 0.01; %set in the range [0.01,1].
+Xinit = real(At(b)); %initialization
+tic
+[X_WTV, cost] = OpWeightedTV_PD_AHMOD2(b,sqrt(edgemask),lambda,A,At,res,Niter,gam,Xinit); %see comments inside OpWeightedTV_PD_AHMOD.m
+toc
+% Show a 2D slice of X_WTV image
+X_WTV_size = size(X_WTV);
+X_WTV_2d = X_WTV(:,:,round(X_WTV_size(3)/2));
+figure(17); imagesc(abs(X_WTV_2d),[0,1]); colorbar; title('hi resolution, WTV');
+figure(18); plot(cost); xlabel('iteration'); ylabel('cost');
+figure(19); imagesc(abs(X_WTV_2d-X0_2d),[0,0.2]); colorbar; title('WTV error image');
+
+SNR_WTV = -20*log10(norm(X_WTV(:)-X0(:))/norm(X0(:)));
+fprintf('WTV (AHMOD) output SNR (Weighted TV algorithm) = %2.1f dB\n',SNR_WTV);
+fprintf('WTV (AHMOD) final cost %6.4f\n',cost(end));
 
 %% Comparison figure
 % labelIFFT = sprintf('SNR=%6.1f',SNR_IFFT);
