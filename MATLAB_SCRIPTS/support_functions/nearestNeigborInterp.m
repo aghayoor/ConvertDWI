b974d2e5c620b61f048303873a40e2b0e816c8d7
Ali Ghayoor <ali-ghayoor@uiowa.edu>
2016-07-06 10:05:10 -0500

    WIP: edit comments for nn

--- MATLAB_SCRIPTS/support_functions/nearestNeigborInterp.m
+++ MATLAB_SCRIPTS/support_functions/nearestNeigborInterp.m
@@ -4,5 +4,5 @@ inres = size(im);
 [r,c,z]=ndgrid(linspace(1,inres(1),outres(1)),...
                linspace(1,inres(2),outres(2)),...
                linspace(1,inres(3),outres(3)));
-im_NN = interp3(im,c,r,z,'nearest'); % c=x, r=y
+im_NN = interp3(im,c,r,z,'nearest'); % c(col)=x, r(row)=y
 end
