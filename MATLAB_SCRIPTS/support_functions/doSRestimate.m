1a618222c46903e9ae06293ef3d5b4da5f5cad1f
Ali Ghayoor <ali-ghayoor@uiowa.edu>
2016-07-06 12:15:30 -0500

    WIP: fixed NN interpolation

--- MATLAB_SCRIPTS/support_functions/doSRestimate.m
+++ MATLAB_SCRIPTS/support_functions/doSRestimate.m
@@ -46,7 +46,7 @@ for c=1:numGrad
     b = A(X0);       %low-resolution fourier samples
     Xlow = real(ifftn(reshape(b,lores)));
     %% Nearest-Neighbor reconstruction
-    X_NN = nearestNeigborInterp(Xlow,res);
+    X_NN = nearestNeigborInterp(NormalizeDataComponent(abs(Xlow)),res);
     estimatedNNsignal(:,:,:,c) = X_NN;
     %% Zero-padded IFFT reconstruction
     X_IFFT = At(fftn(Xlow));
@@ -79,6 +79,52 @@ for c=1:numGrad
     estimatedWTVsignal(:,:,:,c) = X_WTV;
 end
 
+%%
+% Just for sanity check and debugging
+% if(numGrad==1)
+%     X0_size = size(X0);
+%     X0_2d = X0(:,:,round(X0_size(3)/2));
+%     figure(1); imagesc(abs(X0_2d),[0 1]); colorbar; title('ground truth');
+%
+%     edgemap_size = size(edgemap);
+%     edgemap_2d = edgemap(:,:,round(edgemap_size(3)/2));
+%     figure(2); imagesc(edgemap_2d,[0 1]); colorbar; title('spatial weights image');
+%
+%     Xlow_size = size(Xlow);
+%     Xlow_2d = Xlow(:,:,round(Xlow_size(3)/2));
+%     figure(3); imagesc(abs(Xlow_2d)); colorbar; title('low resolution input');
+%
+%     X_NN_size = size(X_NN);
+%     X_NN_2d = X_NN(:,:,round(X_NN_size(3)/2));
+%     figure(4); imagesc(abs(X_NN_2d),[0,1]); colorbar; title('hi resolution, nearest neigbour');
+%
+%     X_IFFT_size = size(X_IFFT);
+%     X_IFFT_2d = X_IFFT(:,:,round(X_IFFT_size(3)/2));
+%     figure(5); imagesc(abs(X_IFFT_2d),[0,1]); colorbar; title('hi resolution, zero-padded IFFT');
+%
+%     X_L2_size = size(X_TV);
+%     X_L2_2d = X_TV(:,:,round(X_L2_size(3)/2));
+%     figure(6); imagesc(abs(X_L2_2d),[0,1]); colorbar; title('hi resolution, TV');
+%
+%     X_WL2_size = size(X_WTV);
+%     X_WL2_2d = X_WTV(:,:,round(X_WL2_size(3)/2));
+%     figure(7); imagesc(abs(X_WL2_2d),[0,1]); colorbar; title('hi resolution, WTV');
+%
+%     SNR_NN = -20*log10(norm(X_NN(:)-X0(:))/norm(X0(:)));
+%     fprintf('Nearest-Neigbour output SNR = %2.1f dB\n',SNR_NN);
+%
+%     SNR_IFFT = -20*log10(norm(X_IFFT(:)-X0(:))/norm(X0(:)));
+%     fprintf('Zero-padded IFFT output SNR = %2.1f dB\n',SNR_IFFT);
+%
+%     SNR_L2 = -20*log10(norm(X_TV(:)-X0(:))/norm(X0(:)));
+%     fprintf('L2 output SNR (L2 algorithm) = %2.1f dB\n',SNR_L2);
+%     fprintf('L2 final cost %2.4e\n',cost(end));
+%
+%     SNR_WL2 = -20*log10(norm(X_WTV(:)-X0(:))/norm(X0(:)));
+%     fprintf('WL2 output SNR (Weighted L2 algorithm) = %2.1f dB\n',SNR_WL2);
+%     fprintf('WL2 final cost %2.4e\n',cost(end));
+% end
+%%
 delete(poolobj)
 
 end
